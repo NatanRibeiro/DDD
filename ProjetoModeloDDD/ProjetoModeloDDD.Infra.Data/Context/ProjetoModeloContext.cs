a8da288b451ad833e3a7af6f887dd904ee9e665a
NatanRibeiro <natan_r.dutra@hotmail.com>
2015-07-20 23:27:34 -0300

    Update: Creating Structure III

---
+++ ProjetoModeloDDD/ProjetoModeloDDD.Infra.Data/Context/ProjetoModeloContext.cs
@@ -0,0 +1,16 @@
+﻿using System.Data.Entity;
+using ProjetoModeloDDD.Domain.Entities;
+
+namespace ProjetoModeloDDD.Infra.Data.Context
+{
+    public class ProjetoModeloContext : DbContext
+    {
+        public ProjetoModeloContext()
+            : base("ProjetoModeloDDD")
+        {
+
+        }
+
+        public DbSet<Cliente> Clientes { get; set; }
+    }
+}
